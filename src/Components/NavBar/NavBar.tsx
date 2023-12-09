64d23e3a67a2eb747d1c18487c52b2ab664f0955
Andrea Siede <andrea.siede@gmail.com>
2023-12-09 13:12:26 -0800

    responsive nav bar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,18 +4,32 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { Button, Link } from '@mui/material';
-import { useRouter } from 'next/navigation';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const pages = [
-  { name: 'ABOUT THE BOOK', ref: '#about-the-book' },
-  { name: 'ABOUT THE AUTHOR', ref: '#about-the-author' },
-  { name: 'CONTACT', ref: '#contact' },
+  { name: 'ABOUT THE BOOK', ref: '#about-the-book', showExcerpt: false },
+  { name: 'ABOUT THE AUTHOR', ref: '#about-the-author', showExcerpt: false },
+  { name: 'CONTACT', ref: '#contact', showExcerpt: false },
+  { name: 'EXCERPT', showExcerpt: true },
 ];
 
-// router.push(`/account/${subNav}`);
-
 const NavBar = ({ toggleShowExcerpt }: { toggleShowExcerpt: any }) => {
-  const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  const handleClick = (show: boolean) => {
+    toggleShowExcerpt(show);
+  };
 
   return (
     <AppBar
@@ -26,7 +40,7 @@ const NavBar = ({ toggleShowExcerpt }: { toggleShowExcerpt: any }) => {
     >
       <Container maxWidth='xl'>
         <Toolbar disableGutters>
-          <Button href={'#nav-bar'} onClick={() => toggleShowExcerpt(false)}>
+          <Button href={'#nav-bar'} onClick={() => handleClick(false)}>
             <Typography
               variant='h4'
               noWrap
@@ -34,7 +48,7 @@ const NavBar = ({ toggleShowExcerpt }: { toggleShowExcerpt: any }) => {
               color='secondary'
               sx={{
                 mr: 2,
-                display: { md: 'flex' },
+                display: { xs: 'none', lg: 'flex' },
                 fontFamily: 'monospace',
                 fontWeight: 700,
                 letterSpacing: '.3rem',
@@ -47,7 +61,71 @@ const NavBar = ({ toggleShowExcerpt }: { toggleShowExcerpt: any }) => {
           <Box
             sx={{
               flexGrow: 1,
-              display: 'flex',
+              display: { xs: 'flex', lg: 'none' },
+            }}
+          >
+            <IconButton
+              size='large'
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleOpenNavMenu}
+              color='inherit'
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', lg: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Button
+                    href={page.ref}
+                    onClick={() => handleClick(page.showExcerpt)}
+                  >
+                    <Typography textAlign='center'>{page.name}</Typography>
+                  </Button>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            href={'#nav-bar'}
+            onClick={() => toggleShowExcerpt(false)}
+            variant='h4'
+            noWrap
+            component='a'
+            color='secondary'
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', lg: 'none' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              textDecoration: 'none',
+            }}
+          >
+            R.M. Finn
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', lg: 'flex' },
               justifyContent: 'space-around',
             }}
           >
@@ -61,23 +139,11 @@ const NavBar = ({ toggleShowExcerpt }: { toggleShowExcerpt: any }) => {
                   fontSize: '20px',
                 }}
                 href={page.ref}
-                onClick={() => toggleShowExcerpt(false)}
+                onClick={() => handleClick(page.showExcerpt)}
               >
                 {page.name}
               </Button>
             ))}
-            <Button
-              key={'excerpt'}
-              sx={{
-                my: 2,
-                color: 'white',
-                display: 'block',
-                fontSize: '20px',
-              }}
-              onClick={() => toggleShowExcerpt(true)}
-            >
-              {'EXCERPT'}
-            </Button>
           </Box>
         </Toolbar>
       </Container>
